7fd22e3ec9bb6448228a902bf04ce9eb68c4341a
Angel gonzalez <unigonzalez078@gmail.com>
2022-12-11 18:54:44 -0500

    Pasando Dto a dto carpeta

--- src/main/java/com/savia/hemofilia/service/impl/EnfermedadesReadServiceImpl.java
+++ src/main/java/com/savia/hemofilia/service/impl/EnfermedadesReadServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @Service
 public class EnfermedadesReadServiceImpl implements EnfermedadesReadService {
+
     @Autowired
     EnfermedadesReadRepository enfermedadesRepository;
 
