abca512edd7bb423797afb2b292f4b19fb7f257d
DESKTOP-Q94H06I\JuanSuarez <juanpablosuarezperdomo1@gmail.com>
2022-12-13 12:01:52 -0500

    Agregando CrossOrigin

--- src/main/java/com/savia/app/controller/CargaDirectaController.java
+++ src/main/java/com/savia/app/controller/CargaDirectaController.java
@@ -6,12 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.savia.app.constants.PathFileUpload;
@@ -22,7 +17,7 @@ import com.savia.app.util.ResponseEntityJson;
 import com.savia.app.valueobject.Message;
 
 @RestController
-@RequestMapping(value = "/documento/v1", produces = "application/json;charset=UTF-8")
+@RequestMapping(value = "/api/v1/documento", produces = "application/json;charset=UTF-8")
 public class CargaDirectaController {
 
     @Autowired
@@ -32,13 +27,11 @@ public class CargaDirectaController {
     private CargaDirectaService cargaDirectaService;
 
     private ResponseEntityJson jsonResponse = new ResponseEntityJson();
-
     @PostMapping("/carga")
     public ResponseEntity<Message> upload(@RequestParam("file") MultipartFile file,
             @RequestParam("cuentaTipo") int cuentaTipo, @RequestParam("ipsEmisora") String ipsEmisora) {
         return (upload.save(file, cuentaTipo, ipsEmisora));
     }
-
     @PostMapping("/carga/bd")
     @ExceptionHandler({ SQLException.class, DataAccessException.class })
     public ResponseEntity<String> uploadDirec(@RequestBody UploadDirectDto uploadDirectDto) {
