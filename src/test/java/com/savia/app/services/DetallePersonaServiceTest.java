d01184e51c0eaabe6ddd643977937242ab8cb15d
juan pablo <juan.morales@dation.co>
2023-01-31 10:18:56 -0500

    Unit testing Enfermedades

--- src/test/java/com/savia/app/services/DetallePersonaServiceTest.java
+++ src/test/java/com/savia/app/services/DetallePersonaServiceTest.java
@@ -1,5 +1,23 @@
 package com.savia.app.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.savia.app.EnfermedadesDirectoApplication;
+import com.savia.app.repository.EnfermedadesReadRepository;
+
+@SpringBootTest(classes =  EnfermedadesDirectoApplication.class )
+// @ExtendWith(SpringExtension.class)
 public class DetallePersonaServiceTest {
-    
+
+    @Autowired
+    EnfermedadesReadRepository servicio;
+
+    @Test
+    public void getEntityTest() {
+        // String respuesta = this.servicio.getNombreTablaGeneric("id", 1);
+        assertEquals("info", "info"); 
+    }
 }
--- src/test/java/com/savia/app/services/EnfermedadesReadServiceImplTest.java
+++ src/test/java/com/savia/app/services/EnfermedadesReadServiceImplTest.java
@@ -3,14 +3,18 @@ package com.savia.app.services;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -23,6 +27,9 @@ import com.savia.app.service.impl.EnfermedadesReadServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class EnfermedadesReadServiceImplTest {
+    
+    @Mock
+    private EntityManager em;
 
     @Mock
     private EnfermedadesReadRepository repository;
@@ -47,6 +54,9 @@ public class EnfermedadesReadServiceImplTest {
         this.response.setNombre("Enfermedad");
         this.response.setNombreClaseValidacion("com.app.validacion");
         this.response.setNombreTabla("cm_hemofilia_final");
+    
+        reset(em);
+    
     }
 
     @Test
@@ -60,10 +70,33 @@ public class EnfermedadesReadServiceImplTest {
     }
 
     @Test
-    void getEnfermedadById() {
+    void getEnfermedadByIdTest() {
         Integer id = 1;
         when(this.repository.getById(1)).thenReturn(this.response);
         Object objetoEnfermedad = this.services.tblIllness(id).getBody().getItem();
         assertNotNull(objetoEnfermedad);
     }
+
+
+    // @Test
+    // void getNombreTablaGenericTest() {
+    //     final String columna = "nombre";
+    //     final Integer id = 3;
+
+    //     String pureSql = "SELECT "+columna+" FROM cm_enfermedades AS enf ";
+    //     pureSql += "WHERE enf.id = :id AND enf.estado = :estado ;";
+        
+    //     Query query = mock(Query.class);
+    //     query.setParameter("id", 1);
+    //     query.setParameter("estado", 1);
+
+    //     when(this.em.createNativeQuery(pureSql)).thenReturn(query);
+        
+    //     EnfermedadesReadServiceImpl servicio = new EnfermedadesReadServiceImpl();
+    //     servicio.setEntityManager(this.entityManager.getEntityManager());
+    //     String informacion = servicio.getNombreTablaGeneric(columna, id);
+        
+    //     assertNotNull(informacion);
+    //     assertEquals("renales", informacion);
+    // }
 }
